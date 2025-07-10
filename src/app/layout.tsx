84d448de57a5aa4de7340d2ad45b7fe83ca65ddd
Cameron Depaoli <camodp267@gmail.com>
2025-07-10 07:05:46 +0000

    Make it a modern dark theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="dark" suppressHydrationWarning>
       <body className={cn('min-h-screen bg-background font-sans antialiased', inter.variable)}>
         <SidebarProvider>
           <div className="flex">
