eb67911f0e05ddd8e31f63d2fa8880d74358f6fa
Cameron Depaoli <camodp267@gmail.com>
2025-07-11 04:13:35 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,7 @@
 
 'use client';
 
+import * as React from 'react';
 import { useState, useMemo } from 'react';
 import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
