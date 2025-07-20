f2ad633ca20a03905e08cb639b429734b288d1b5
Cameron Depaoli <camodp267@gmail.com>
2025-07-20 01:30:01 +0000

    The side bar isnt auto hiding?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
             <AppSidebar />
             <SidebarInset>
               {children}
-               <div className="md:hidden fixed bottom-4 left-4 z-20">
+               <div className="md:hidden fixed bottom-4 left-4 z-50">
                 <SidebarTrigger asChild>
                     <Button size="lg" className="rounded-full h-14 w-14 shadow-lg">
                         <Menu className="h-6 w-6" />
